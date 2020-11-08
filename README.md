0782864801aad149bc3e2f7e94aa8e3abaa5c03a
adela <adelabbok@gmail.com>
2020-11-09 02:55:19 +0900

    :hammer:Refactor:Add Finished List - Add functions to move TODO-items
    --- COMMIT END ---

--- README.md
+++ README.md
@@ -1,29 +1,31 @@
 # to-do-list.github.io
 
 ### Chrome Application - Momentum (clone)
+
 Personal Dashboard New Tab Chrome Extension
 
 ## Features
+
 - [x] To Do List
 - [x] Weather
-- [ ] Daily Quote
-- [ ] Animation
-- [ ] Offline Support
 
-----
+---
+
 ## Try it Out
 
-https://bbodela.github.io/to-do-list.github.io/
-----
+## https://bbodela.github.io/to-do-list.github.io/
+
 ### Implementation Completed
+
 1. Clock
 2. Render name entered by user
 3. To-do-List
-  - Just render To-Do-lists
-  - Save To-Do-lists to window.localStorage
-  - Click the Delete button of 'To-Do' to delete the 'To-Do'
+
+- Just render To-Do-lists
+- Save To-Do-lists to window.localStorage
+- Click the Delete button of 'To-Do' to delete the 'To-Do'
+
 4. Background Random Images
 5. Access to Latitude and Longitude
 6. Weather API
 7. Modularize the key (using Webpack)
-
--- todo.js
+++ todo.js
@@ -1,78 +1,158 @@
-const toDoForm = document.querySelector(".js-toDoForm");
-const toDoInput = toDoForm.querySelector("input");
-const toDoList = document.querySelector(".js-toDoList");
+const toDoForm = document.querySelector(".js-toDoForm"),
+	toDoInput = toDoForm.querySelector("input"),
+	toDoList = document.querySelector(".js-toDoList"),
+	finishList = document.querySelector(".js-finishList");
 
-const LS_TODOS = "toDos";
+const LS_TODOS = "TODOS";
+const LS_FINISH = "FINISHED";
 
-let toDos = [];
+let toDosArr = [];
+let finishedArr = [];
 
-function deleteToDo(e){
-  const btn = e.target;
-  const li = btn.parentNode;
-  toDoList.removeChild(li); //요까지만 하면 localStorage에서 삭제된것이 아니라서 새로고침하면 다시 뜬다
-  const cleanToDos = toDos.filter(toDo => 
-    // console.log(toDo.id, li.id)
-    toDo.id !== parseInt(li.id)
-  )
-  toDos = cleanToDos;
-  saveToDos();
+function loadToDos() {
+	const lodedToDos = localStorage.getItem(LS_TODOS);
+	// if(toDosArr === null){}
+	// 저장된 투두가 없으면 아무것도 안한다 todo 공간은 항상 showing일 것이기 때문
+	if (lodedToDos !== null) {
+		// 저장된 투두를 뿌려줌
+		const parsedToDos = JSON.parse(lodedToDos);
+		parsedToDos.forEach(toDo => renderToDo(toDo.text));
+	}
+}
+function loadFinish() {
+	const loadedFinish = localStorage.getItem(LS_FINISH);
+	if (loadedFinish !== null) {
+		const parsedFinish = JSON.parse(loadedFinish);
+		parsedFinish.forEach(function (check) {
+			renderFinish(check.text);
+		});
+	}
 }
 
-function saveToDos(){
-  localStorage.setItem(LS_TODOS, JSON.stringify(toDos));
+function saveToDos() {
+	localStorage.setItem(LS_TODOS, JSON.stringify(toDosArr));
+}
+function saveFinish() {
+	localStorage.setItem(LS_FINISH, JSON.stringify(finishedArr));
 }
 
-function renderToDo(text){
-  // ul태그에 li달아주기
-  const li = document.createElement("li");
-  const delBtn = document.createElement("button");
-  const span = document.createElement("span");
-  const newId = toDos.length + 1;
+function renderToDo(text) {
+	// ul태그에 li달아주기
+	const li = document.createElement("li");
+	const delBtn = document.createElement("button");
+	const checkBtn = document.createElement("button");
+	const span = document.createElement("span");
+	const newId = toDosArr.length + 1;
 
-  delBtn.addEventListener("click", deleteToDo);
-  delBtn.innerText = "✖️";
-  span.innerText = text;
-  delBtn.classList.add("delBtn")
-  li.classList.add("todo")
-  li.appendChild(span);
-  li.appendChild(delBtn);
-  li.id = newId;
-  toDoList.appendChild(li);
-  // 여기까지 화면에 html제어
-  const toDoObj = {
-    text: text,
-    id: newId
-  }
-  toDos.push(toDoObj);
-  saveToDos();
+	delBtn.addEventListener("click", deleteToDo);
+	checkBtn.addEventListener("click", checkToDo);
+	delBtn.innerText = "✖️";
+	checkBtn.innerText = "✅";
+	span.innerText = text;
+
+	delBtn.classList.add("delBtn");
+	checkBtn.classList.add("checkBtn");
+	li.classList.add("todo");
+
+	li.appendChild(span);
+	li.appendChild(delBtn);
+	li.appendChild(checkBtn);
+
+	li.id = newId;
+	toDoList.appendChild(li);
+	// 여기까지 화면에 html제어
+	const toDoObj = {
+		text: text,
+		id: newId,
+	};
+	toDosArr.push(toDoObj);
+	saveToDos();
+}
+function renderFinish(text) {
+	const li = document.createElement("li");
+	const delBtn = document.createElement("button");
+	const checkBtn = document.createElement("button");
+	const span = document.createElement("span");
+	const newId = toDosArr.length + 1;
+
+	delBtn.innerText = "✖️";
+	checkBtn.innerText = "↩️";
+	delBtn.addEventListener("click", deleteCheck);
+	checkBtn.addEventListener("click", returnToDo);
+	span.innerText = text;
+
+	delBtn.classList.add("delBtn");
+	checkBtn.classList.add("checkBtn");
+	li.classList.add("todo");
+
+	li.appendChild(span);
+	li.appendChild(delBtn);
+	li.appendChild(checkBtn);
+	li.id = newId;
+	finishList.appendChild(li);
+	const finishObj = {
+		text: text,
+		id: newId,
+	};
+	finishedArr.push(finishObj);
+	saveFinish();
 }
 
-function handleSubmit(event){
-  event.preventDefault();
-  // toDoInput.value를 toDoList에 붙여줌 & localStorage저장
-  const currentValue = toDoInput.value;
-  renderToDo(currentValue);
-  toDoInput.value = "";
+function deleteToDo(e) {
+	const btn = e.target;
+	const li = btn.parentNode;
+	toDoList.removeChild(li); //요까지만 하면 localStorage에서 삭제된것이 아니라서 새로고침하면 다시 뜬다
+	const cleanToDos = toDosArr.filter(
+		toDo =>
+			// console.log(toDo.id, li.id)
+			toDo.id !== parseInt(li.id)
+	);
+	toDosArr = cleanToDos;
+	saveToDos();
+}
+function deleteCheck(e) {
+	const btn = e.target;
+	const li = btn.parentNode;
+	finishList.removeChild(li);
+	const cleanChecks = finishedArr.filter(todo => todo.id !== Number(li.id));
+	finishedArr = cleanChecks;
+	saveFinish();
 }
 
-function submitToDos(){
-  toDoForm.addEventListener("submit", handleSubmit);
+function checkToDo(e) {
+	const btn = e.target;
+	const li = btn.parentNode;
+	toDoList.removeChild(li);
+	const delTodos = toDosArr.filter(todo => todo.id !== parseInt(li.id));
+	toDosArr = delTodos;
+	saveToDos();
+	renderFinish(li.childNodes[0].innerText);
+}
+function returnToDo(e) {
+	const btn = e.target;
+	const li = btn.parentNode;
+	finishList.removeChild(li);
+	const cleanChecks = finishedArr.filter(todo => {
+		return todo.id !== parseInt(li.id);
+	});
+	finishedArr = cleanChecks;
+	saveFinish();
+	renderToDo(li.childNodes[0].innerText);
 }
 
-function loadToDos(){
-  const lodedToDos = localStorage.getItem(LS_TODOS);
-  // if(toDos === null){} 
-    // 저장된 투두가 없으면 아무것도 안한다 todo 공간은 항상 showing일 것이기 때문
-  if(lodedToDos !== null) {
-    // 저장된 투두를 뿌려줌
-    const parsedToDos = JSON.parse(lodedToDos);
-    parsedToDos.forEach(toDo => renderToDo(toDo.text));
-  }
+function handleSubmit(event) {
+	event.preventDefault();
+	const currentValue = toDoInput.value;
+	renderToDo(currentValue);
+	toDoInput.value = "";
+}
+function submitToDos() {
+	toDoForm.addEventListener("submit", handleSubmit);
 }
 
-function init(){
-  loadToDos();
-  submitToDos();
+function init() {
+	loadToDos();
+	submitToDos();
 }
 
 init();
